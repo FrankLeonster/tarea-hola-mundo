37d3d7dd25e5e350d915d0168f4647d65690767f
Frank Leon <fa_bian15@hotmail.com>
2016-08-29 21:07:16 -0500

    Hola Mundo - Primer Tarea

---
+++ AndroidStudioProjects/HolaATodos/app/src/main/java/com/example/frank/holaatodos/HolaMundoTarea.java
@@ -0,0 +1,13 @@
+package com.example.frank.holaatodos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HolaMundoTarea extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hola_mundo_tarea);
+    }
+}
